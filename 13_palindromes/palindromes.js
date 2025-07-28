bcc9de807430b2e2fc62a8394a7577ef6164b9cf
Faizul <faizulmd1306@gmail.com>
2025-07-28 20:26:37 +0530

    Finish palindrome exercise

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,12 @@
-const palindromes = function () {
-
+const palindromes = function (words) {
+  const alphanumerical = "abcdefghijklmnopqrstuvwxyz1234567890";
+  const string = words
+    .toLowerCase()
+    .split("")
+    .filter((item) => alphanumerical.includes(item))
+    .join("");
+  const reversedString = string.split("").reverse().join("");
+  return string === reversedString;
 };
 
 // Do not edit below this line
