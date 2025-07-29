6ae26443af2e06e1c7c2320179f1266990423ac5
Faizul <faizulmd1306@gmail.com>
2025-07-29 19:15:04 +0530

    Finish find the oldest exercise

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
+function findAge(person) {
+  let yearOfDeath = person.yearOfDeath;
+  let yearOfBirth = person.yearOfBirth;
+  if (person.yearOfDeath === undefined) {
+    yearOfDeath = new Date().getFullYear();
+  }
+  return yearOfDeath - yearOfBirth;
+}
 
+const findTheOldest = function (people) {
+  let agedPeople = people.sort((personA, personB) => {
+    return findAge(personB) - findAge(personA);
+  });
+
+  return agedPeople[0];
 };
 
 // Do not edit below this line
